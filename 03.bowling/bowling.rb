90f5025e52581a3dff82b5b9190171e996644837
ozasa <kokioza2001@icloud.com>
2023-04-12 17:45:57 +0900

    修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
-shots = scores.map { |s| s == 'X' ? 'X' : s.to_i }
+input_num = ARGV[0]
+score = input_num.split(',')
+shots = score.map { |s| s == 'X' ? 'X' : s.to_i }
 
-frames = []
-10.times { frames << [] }
+frames = Array.new(10) { [] }
+
+LAST_FRAME_INDEX = 9
+STRIKE = 10
 
 frames_index = 0
 shots.each do |shot|
-  if frames_index == 9
-    frames[frames_index] << if shot == 'X'
-                              10
-                            else
-                              shot
-                            end
+  if frames_index == LAST_FRAME_INDEX
+    frames[frames_index] << (shot == 'X' ? STRIKE : shot)
   elsif shot == 'X'
-    frames[frames_index] << 10
+    frames[frames_index] << STRIKE
     frames_index += 1
   else
     frames[frames_index] << shot
@@ -29,9 +27,9 @@ total_point = 0
 frames.each_with_index do |frame, index|
   total_point += frames[index].sum
 
-  if index == 9
+  if index == LAST_FRAME_INDEX
     break
-  elsif frame[0] == 10
+  elsif frame[0] == STRIKE
     total_point += if index + 1 == 9
                      frames[index + 1][0] + frames[index + 1][1]
                    elsif frames[index + 1][0] == 10
